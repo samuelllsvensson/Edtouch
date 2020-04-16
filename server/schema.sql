89e2b2e17940376a2d29528b62a4ef6463af1715
Samuel Svensson <samuel.svensson.95@gmail.com>
2020-04-16 15:07:36 +0200

    Updated schema

--- server/schema.sql
+++ server/schema.sql
@@ -1,26 +1,45 @@
 
--- CREATE TABLE users (
---   uid SERIAL PRIMARY KEY,
---   username VARCHAR(255) UNIQUE,
---   email VARCHAR(255),
---   email_verified BOOLEAN,
---   date_created DATE,
---   last_login DATE
--- );
+-- TODO: profile_image_url, total_likes?
+CREATE TABLE users (
+  user_id SERIAL PRIMARY KEY,
+  username VARCHAR(255) UNIQUE,
+  name VARCHAR(255),
+  email VARCHAR(255),
+  date_created DATE
+);
 
-
+-- TODO: image_url, likes
 CREATE TABLE posts (
   post_id SERIAL PRIMARY KEY,
   title VARCHAR(255),
   body VARCHAR,
+  author_id VARCHAR REFERENCES users(user_id),
+  date_created TIMESTAMP,
+);
+
+CREATE TABLE post_comments (
+  comment_id SERIAL PRIMARY KEY,
+  body VARCHAR(255),
+  username VARCHAR REFERENCES users(username),
+  user_id INT REFERENCES users(user_id),
+  post_id INT REFERENCES posts(post_id),
   date_created TIMESTAMP
 );
 
--- CREATE TABLE comments (
---   cid SERIAL PRIMARY KEY,
---   comment VARCHAR(255),
+-- TODO: image_url, likes
+-- CREATE TABLE edits (
+--   edit_id SERIAL PRIMARY KEY,
+--   title VARCHAR(255),
+--   body VARCHAR,
 --   author VARCHAR REFERENCES users(username),
---   user_id INT REFERENCES users(uid),
---   post_id INT REFERENCES posts(pid),
+--   date_created TIMESTAMP
+-- );
+
+-- CREATE TABLE edit_comments (
+--   edit_comment_id SERIAL PRIMARY KEY,
+--   body VARCHAR(255),
+--   author VARCHAR REFERENCES users(username),
+--   user_comment_id INT REFERENCES users(user_id),
+--   edit_comment_id INT REFERENCES edits(edit_id),
 --   date_created TIMESTAMP
 -- );
