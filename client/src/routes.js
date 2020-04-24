073b4bc301ddd2e8618bad971514b41c213c88bd
johanforslund <johanforslund95@gmail.com>
2020-04-24 16:23:01 +0200

    Now possible to login via auth0

--- client/src/routes.js
+++ client/src/routes.js
@@ -1,12 +1,17 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Router, Route, Switch } from "react-router";
+import Context from "./utils/context";
 import history from "./utils/history";
 import Home from "./views/Home";
 import Post from "./views/Post";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Callback from "./components/Callback";
+import AuthCheck from "./utils/AuthCheck";
 
 const Routes = () => {
+  const context = useContext(Context);
+
   return (
     <div>
       <Router history={history}>
@@ -15,6 +20,14 @@ const Routes = () => {
           <Switch>
             <Route exact path="/" component={Home} />
             <Route path="/post/:post_id" component={Post} />
+            <Route
+              path="/callback"
+              render={(props) => {
+                context.handleAuth(props);
+                return <Callback />;
+              }}
+            />
+            <Route path="/authcheck" component={AuthCheck} />
           </Switch>
         </div>
       </Router>
